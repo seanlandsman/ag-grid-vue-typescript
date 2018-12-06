0f503c832e84416bd1c4d30723370765fa4752ff
Sean Landsman <sean@thelandsmans.com>
2018-12-06 11:38:52 +0000

    Update to use new ag-grid-vue

--- src/App.ts
+++ src/App.ts
@@ -1,10 +1,8 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
-import AgGridVue from 'ag-grid-vue';
+import {AgGridVue} from 'ag-grid-vue';
 
-// The @Component decorator indicates the class is a Vue component
 @Component({
-    // All component options are allowed in here
     template: `
         <ag-grid-vue style="width: 500px; height: 500px;"
                  class="ag-theme-balham"
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import Vue from 'vue'
 // import App from './App.vue'
 
 // ts component
+// if using this ensure "runtimeCompiler: true" is set in vue.config.js as the template needs to be compiled
+// in a full app the template would be precompiled
+// this property can be removed/commented out when doing a prod build though as the template will be compiled during that
+// process
 import App from './App'
 
 import "../node_modules/ag-grid-community/dist/styles/ag-grid.css";
